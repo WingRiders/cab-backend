7d54cb97061a2a8c38cdf0894dce009ce78ebc5b
TechRiderWR <techrider@wingriders.com>
2024-03-22 16:25:55 +0100

    Add getUtxos endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {Elysia} from 'elysia'
 import {config} from './config'
+import {getUTxOs} from './ogmios'
 
 export const app = new Elysia()
 	.get('/healthcheck', () => ({healthy: true, uptime: process.uptime()}))
+	.get('/utxos/:address', ({params: {address}}) => getUTxOs({addresses: [address]}))
 	.listen(config.PORT)
