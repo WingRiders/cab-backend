0779e3ced8f929d88e401331dba440ad665737db
TechRiderWR <techrider@wingriders.com>
2024-03-22 14:00:12 +0100

    Add linting

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 Bun.serve({
-  port: process.env.PORT || 3000,
-  fetch: (_, __) => new Response('Hello World!'),
+	port: process.env.PORT || 3000,
+	fetch: (_, __) => new Response('Hello World!'),
 })
